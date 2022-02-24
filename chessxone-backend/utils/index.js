bef75b19fbdc09905ede498e935b3edc085ed34b
bouguettaya ayoub <ayoubbouguettaya1616@gmail.com>
2022-02-24 22:15:01 +0100

    rewrite GenerateTagID and fix behaviour issue

--- chessxone-backend/utils/index.js
+++ chessxone-backend/utils/index.js
@@ -1,16 +1,45 @@
 const mongoose = require('mongoose');
-const redis = require('./redisCommand')
+const redis = require('./redisCommand');
+const User = require('../api/models/user')
 
 const { TAG_ID_OFFSET } = require("./redisKeys");
 
-const generateNewTagID = async () => {
-    await redis.incr(TAG_ID_OFFSET)
-    const tagIDOffset = await redis.get(TAG_ID_OFFSET);
-    console.log(tagIDOffset)
-    return (Number.parseInt(tagIDOffset) + 1234567);
+const randomCaracters = (length = 3) => {
+    const caracters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let randomString = ''
+    for (let i = 0; i < length; i++) {
+        randomString += caracters.charAt(Math.floor(Math.random() * caracters.length));
+    }
+    return randomString;
+}
+
+const findUserBytagID = (tagIdParams) => User.findOne({ tagID: tagIdParams }).lean()
+
+const generateNewTagID = async (customFindByTagID = null) => {
+    let isFound = false;
+    do {
+        const tagIDOffset = await redis.get(TAG_ID_OFFSET) || 1;
+        if (Number.parseInt(tagIDOffset) === 8999) {
+            await redis.set(TAG_ID_OFFSET, 1)
+        } else {
+            await redis.incr(TAG_ID_OFFSET)
+        }
+
+        const randomSide = randomCaracters(3)
+        const offsetSide = Number.parseInt(tagIDOffset) + 1000;
+
+        tagID = randomSide + offsetSide;
+/*
+is a case of testing we will need this custom_Find_By_tag_ID function
+*/
+        isFound = customFindByTagID ? await customFindByTagID(tagID) : await findUserBytagID(tagID)
+
+    } while (isFound);
+
+    return tagID
 };
 
-const generateNewObjectID = async () => {return await new mongoose.Types.ObjectId();}
+const generateNewObjectID = async () => { return await new mongoose.Types.ObjectId(); }
 
 const generate_IncomingMatchRequests_Key = (userID) => (`INCOMING_MATCH_REQS_${userID}`);
 const generate_OutgoingMatchRequest_Key = (userID) => (`OUTGOING_MATCH_REQ_${userID}`);
